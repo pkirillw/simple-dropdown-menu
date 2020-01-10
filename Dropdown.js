fa98f931fdb03bb143b82983af11bcd55080d9f5
pkirillw <pkirillw@gmail.com>
2020-01-10 16:07:37 +0300

    Add data-dropdown-menu-listener-enter and data-dropdown-menu-listener-leave. With these attributes you can change listener for each dropdown.

--- Dropdown.js
+++ Dropdown.js
@@ -9,17 +9,22 @@ export default class Dropdown {
         options.listenerLeave = options.listenerLeave || 'mouseout';
 
         Array.from(document.querySelectorAll('*[data-dropdown-menu]')).forEach(((value) => {
-                value.addEventListener(options.listenerEnter, () => {
+                let listenerEnter = value.getAttribute('data-dropdown-menu-listener-enter') || options.listenerEnter;
+                let listenerLeave = value.getAttribute('data-dropdown-menu-listener-leave') || options.listenerLeave;
+
+                value.addEventListener(listenerEnter, () => {
                     let dropdownMenu = document.getElementById(value.getAttribute('data-dropdown-menu'));
                     dropdownMenu.classList.toggle("d-block");
+
                     if (dropdownMenu.getAttribute('have-listener') !== 'true') {
-                        dropdownMenu.addEventListener(options.listenerLeave, () => {
+                        dropdownMenu.addEventListener(listenerLeave, () => {
                             dropdownMenu.classList.toggle("d-block");
                         })
                     }
                     dropdownMenu.setAttribute('have-listener', 'true');
                 });
-                value.addEventListener(options.listenerLeave, (e) => {
+
+                value.addEventListener(listenerLeave, (e) => {
                         if (!(e.toElement.id && document.querySelector('*[data-dropdown-menu=' + e.toElement.id + ']'))) {
                             document.getElementById(value.getAttribute('data-dropdown-menu')).classList.remove('d-block');
                         }
